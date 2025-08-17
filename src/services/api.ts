1a3285c1cc37c5f9e709a74cf3a72d5534eb2a26
VillaGoll <villagolvn@gmail.com>
2025-08-16 21:06:26 -0600

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // temporalmente 5001 mientras 5000 está ocupado
+    baseURL: 'https://backenvillagolbackend.fly.dev/api'
     headers: {
         'Content-Type': 'application/json',
     },
